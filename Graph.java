06123f81685b5f12a34c2cfabb52588da348165d
Aria <chan11@dons.usfca.edu>
2023-12-06 15:00:25 -0800

    get distance working

--- Graph.java
+++ Graph.java
@@ -9,38 +9,66 @@ import java.util.Set;
 public class Graph {
     // use Integer max value instead of infinity for path and cost
     private final int INFINITY = Integer.MAX_VALUE;
-    /* 
+
     HashMap<String, Integer> capDistMap = new HashMap<String, Integer>();
     HashMap<String, String> stateNameMap = new HashMap<String, String>();
     HashMap<String, List<String>> bordersMap = new HashMap<String, List<String>>();
     HashMap<String, String> reverseStateMap = new HashMap<String, String>();
-    */
-    // full graph, with a country name (code) as the key and a hashmap of adjacent countries and their distances as the value
+    HashMap<String, String> edgeCases = new HashMap<String, String>();
+    HashMap<String, String> edgeCasesbyString = new HashMap<String, String>();
+
+    // full graph, with a country name (code) as the key and a hashmap of adjacent
+    // countries and their distances as the value
     HashMap<String, HashMap<String, Integer>> graph = new HashMap<String, HashMap<String, Integer>>();
 
-    public Graph() {
-    }
-
-    // method to build the graph from data (hashmaps)
-    public void buildGraph(
-            HashMap<String, Integer> capDistMap,
+    public Graph(HashMap<String, Integer> capDistMap,
             HashMap<String, String> stateNameMap,
             HashMap<String, List<String>> bordersMap,
             HashMap<String, String> reverseStateMap,
             HashMap<String, String> edgeCases,
             HashMap<String, String> edgeCasesbyString) {
+        // countries and their distances as the value
+        this.graph = new HashMap<String, HashMap<String, Integer>>();
+        this.capDistMap = capDistMap;
+        this.stateNameMap = stateNameMap;
+        this.bordersMap = bordersMap;
+        this.reverseStateMap = reverseStateMap;
+        this.edgeCases = edgeCases;
+        this.edgeCasesbyString = edgeCasesbyString;
+    }
+
+    public int returnGraphDist(String country1, String country2) {
+        // initialize distance
+        int distance = 0;
+        // get code for country name
+        String countryCode = reverseStateMap.get(country1);
+        String countryCode2 = reverseStateMap.get(country2);
+        HashMap<String, Integer> borderCountries = this.graph.get(countryCode);
+        Set<String> codeKeys = borderCountries.keySet();
+        for (String country : codeKeys) {
+            if (country.equals(country2)) {
+                // if the country is found, get the distance
+                distance = borderCountries.get(country);
+            }
+        }
+
+        return distance;
+    }
+
+    // method to build the graph from data (hashmaps)
+    public void buildGraph() {
 
         System.out.println("BuildGraph function entered");
 
         // Get all of the country codes from the state name hashmap
         Set<String> countryCode = stateNameMap.keySet();
-        //System.out.println(countryCode);
+        // System.out.println(countryCode);
 
         // Loop over the country codes
         for (String country : countryCode) {
             // Get the country name
             String countryString = stateNameMap.get(country);
-            //  System.out.println("Country string: " + countryString);
+            // System.out.println("Country string: " + countryString);
             // Get list of bordering countries
             List<String> borderingCountries = null;
             borderingCountries = bordersMap.get(countryString);
@@ -51,45 +79,47 @@ public class Graph {
                 // if it is an edge case
                 if (countryasBorderString != null) {
                     borderingCountries = bordersMap.get(countryasBorderString);
-                //    System.out.println("edge case: " + countryasBorderString);
+                    // System.out.println("edge case: " + countryasBorderString);
                 } else {
                     continue;
                 }
             }
             // check if bordering countries are still null
             if (borderingCountries != null) {
-
                 // loop over bordering countries, get codes, and add to graph
                 for (String borderingCountry : borderingCountries) {
                     // create inner hashmap with strings for each country and corresponding integers
                     HashMap<String, Integer> innerHashMap = graph.get(country);
-if (innerHashMap == null) {
-    innerHashMap = new HashMap<String, Integer>();
-}
+                    if (innerHashMap == null) {
+                        innerHashMap = new HashMap<String, Integer>();
+                    }
                     // System.out.println( "bordering country name: " + borderingCountry);
                     // get country code for bordering country string
-                    //System.out.println("bordering country: " + borderingCountry);
+                   // System.out.println("bordering country: " + borderingCountry);
                     String borderingCountryCode = reverseStateMap.get(borderingCountry);
+                   // System.out.println("bordering country code: " + borderingCountryCode);
                     if (borderingCountryCode == null) {
                         checkEdgeCasebyString(borderingCountry, edgeCasesbyString);
                     }
-                //    System.out.println(borderingCountryCode + " is the code for " + borderingCountry);
+                    // System.out.println(borderingCountryCode + " is the code for " +
+                    // borderingCountry);
                     // add each bordering country to the inner hashmap
                     // get distance
-                    //  System.out.println(country + "_" + borderingCountryCode);
+                    // System.out.println(country + "_" + borderingCountryCode);
                     // get key
                     String key = country + "_" + borderingCountryCode;
                     Integer distance = capDistMap.get(key);
-                 //   System.out.println(key + " at " + distance);
+                    // System.out.println(key + " at " + distance);
                     innerHashMap.put(borderingCountry, distance);
-                    // System.out.println("country " + country + " bordering country " + borderingCountryCode);
+                    // System.out.println("country " + country + " bordering country " +
+                    // borderingCountryCode);
                     // put inner hashmap into the graph
                     graph.put(country, innerHashMap);
-                     System.out.println(country + ": " + innerHashMap);
+                    // System.out.println(country + ": " + innerHashMap);
                 }
             }
         }
-        }
+    }
 
     public String checkEdgeCasebyCode(String countryCode, HashMap<String, String> edgeCases) {
         // check if given country code is in edge cases
@@ -108,12 +138,14 @@ if (innerHashMap == null) {
         // if not in edge cases, return null
         return null;
     }
-     public String checkEdgeCasebyString(String countryString, HashMap<String, String> edgeCasesbyString) {
+
+    public String checkEdgeCasebyString(String countryString, HashMap<String, String> edgeCasesbyString) {
         // check if given country code is in edge cases
         for (String string : edgeCasesbyString.keySet()) {
             if (string.equals(countryString)) {
                 // get the corresponding code
                 String code = edgeCasesbyString.get(string);
+            //    System.out.println(countryString + code);
                 // return the code
                 return code;
             }
@@ -121,36 +153,48 @@ if (innerHashMap == null) {
         // if not in edge cases, return null
         return null;
     }
-    
+
     public void printGraph() {
         for (String country : graph.keySet()) {
             System.out.println(country + ": " + graph.get(country));
         }
     }
 
-    // Dijkstra's algorithm to find the shortest path (courtesy of ChatGPT)
+    // Dijkstra's algorithm to find the shortest path (polished by ChatGPT)
     public List<String> dijkstra(String source, String destination) {
         Map<String, Integer> distances = new HashMap<>();
         Map<String, String> predecessors = new HashMap<>();
         PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-
+        // put all of the countries in the graph into the queue, initializing all at
+        // infinity
         for (String country : graph.keySet()) {
-            distances.put(country, country.equals(source) ? 0 : INFINITY);
+            if (country.equals(source)) {
+                distances.put(country, 0);
+            } else {
+                distances.put(country, INFINITY);
+            }
+            // add to priority queue
             pq.offer(new HashMap.SimpleEntry<>(country, distances.get(country)));
         }
-
+        // loop to the end of the queue
         while (!pq.isEmpty()) {
+            // get next country in the queue, which is the lowest cost unknown vertext
             String currentCountry = pq.poll().getKey();
             int currentDistance = distances.get(currentCountry);
-
-            if (currentDistance == INFINITY) continue;
-            if (currentCountry.equals(destination)) break;
-
+            // set known, which is the removal from the queue in this case
+            // the distance being infinity marks it as unknown
+            if (currentDistance == INFINITY)
+                continue;
+            // stop when the destination is reached
+            if (currentCountry.equals(destination))
+                break;
+            // for each neighbor, update cost (cost to get to current) + cumilative distance
             for (Map.Entry<String, Integer> neighborEntry : graph.get(currentCountry).entrySet()) {
                 String neighbor = neighborEntry.getKey();
                 int edgeWeight = neighborEntry.getValue();
+                // total distance + distance between the two specific entries
                 int newDistance = currentDistance + edgeWeight;
-
+                // if the new distance is less than the prev distance, update it
                 if (newDistance < distances.get(neighbor)) {
                     distances.put(neighbor, newDistance);
                     predecessors.put(neighbor, currentCountry);
@@ -163,15 +207,18 @@ if (innerHashMap == null) {
     }
 
     // Method to reconstruct the shortest path from source to destination
+    // aka go backwards
     private List<String> reconstructPath(Map<String, String> predecessors, String source, String destination) {
+        // list to store the path
         LinkedList<String> path = new LinkedList<>();
+        // string to store each step in the path
         String step = destination;
 
         // Check if a path exists
         if (predecessors.get(step) == null) {
             return path; // empty list if no path is found
         }
-
+        // if it does, add it
         path.add(step);
         while (predecessors.containsKey(step) && !step.equals(source)) {
             step = predecessors.get(step);
@@ -186,6 +233,3 @@ if (innerHashMap == null) {
         return path;
     }
 }
-
-
-
--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class IRoadTrip {
   // list to hold all countries
-    public List<String> totalCountryList = new ArrayList<String>();
+  public List<String> totalCountryList = new ArrayList<String>();
 
   String country1;
   String country2;
@@ -16,11 +15,14 @@ public class IRoadTrip {
   HashMap<String, String> reverseStateMap = new HashMap<String, String>();
   HashMap<String, String> edgeCases = new HashMap<String, String>();
   HashMap<String, String> edgeCasesbyString = new HashMap<String, String>();
+
   /*
    * Constructor
+   * 
    * @param args command line arguments
    * files: borders.txt capdist.csv state_name.tsv.
-   * Read the files and prepare to execute the implementation to respond to requests.
+   * Read the files and prepare to execute the implementation to respond to
+   * requests.
    * The implementation must halt on any failure here.
    */
   public IRoadTrip(String[] args) {
@@ -31,17 +33,24 @@ public class IRoadTrip {
       String capdist = args[1].trim();
       String borders = args[2].trim();
       // check file names
-      if (state_name.equals("state_name.tsv") && capdist.equals("capdist.csv") && borders.equals("borders.txt") ) {
-       System.out.println("Filenames validated, starting reading...");
+      if (state_name.equals("state_name.tsv") && capdist.equals("capdist.csv") && borders.equals("borders.txt")) {
+        System.out.println("Filenames validated, starting reading...");
         // create object to read files
-        readFiles files = new readFiles(stateNameMap, capDistMap, bordersMap, reverseStateMap, edgeCases, edgeCasesbyString);
-         System.out.println("Files read, printing...");
-         // Printing elements in object of Map
-    //    System.out.println("reverse State Map " + reverseStateMap + "\n");
-    //   System.out.println("State Map " + stateNameMap);
-       // System.out.println("Cap Dist Map " + capDistMap);
-       //  System.out.println(bordersMap);
-      System.out.println("Printing edge cases..." + edgeCases);
+        readFiles files = new readFiles(
+            stateNameMap,
+            capDistMap,
+            bordersMap,
+            reverseStateMap,
+            edgeCases,
+            edgeCasesbyString);
+        System.out.println("Files read, printing...");
+        // Printing elements in object of Map
+        // System.out.println("reverse State Map " + reverseStateMap + "\n");
+        // System.out.println("State Map " + stateNameMap);
+        // System.out.println("Cap Dist Map " + capDistMap);
+        // System.out.println(bordersMap);
+
+        // System.out.println("Printing edge cases..." + edgeCases);
       } else {
         System.out.println("Invalid file names");
         System.out.println("state_name: " + state_name);
@@ -53,24 +62,29 @@ public class IRoadTrip {
     }
   }
 
-  /* This function provides the shortest path distance between the capitals of the two countries passed as arguments. 
-    If either of the countries does not exist or if the countries do not share a land border, 
-    this function must return a value of -1.
-    */
-  public int getDistance(String country1, String country2) {
-    //initialize distance to be -1
+  /*
+   * This function provides the shortest path distance between the capitals of the
+   * two countries passed as arguments.
+   * If either of the countries does not exist or if the countries do not share a
+   * land border,
+   * this function must return a value of -1.
+   */
+  public int getDistance(String country1, String country2, Graph graph) {
+    // initialize distance to be -1
     int distance = -1;
     // get both country names
     // get each country code from name
     try {
-      String country1Code = stateNameMap.get(country1);
-      String country2Code = stateNameMap.get(country2);
-      // System.out.println(country1Code + " " + country2Code);
-      /*  use country codes to get distance
-      distance = capDistMap.get((String) country1Code + "_" + (String) country2Code);
-      System.out.println(distance);
-      System.out.println("distance: " + distance);
-      */ 
+      distance = graph.returnGraphDist(country1, country2);
+      // get distance from main graph
+
+      /*
+       * use country codes to get distance
+       * distance = capDistMap.get((String) country1Code + "_" + (String)
+       * country2Code);
+       * System.out.println(distance);
+       * System.out.println("distance: " + distance);
+       */
       return distance;
     } catch (Exception e) {
       System.out.println("Error in getDistance: " + e);
@@ -79,11 +93,15 @@ public class IRoadTrip {
   }
 
   /*
-   * This function determines and returns a path between the two countries passed as arguments.
+   * This function determines and returns a path between the two countries passed
+   * as arguments.
    * This path must start in country1 and end in country 2.
-   * If either of the countries does not exist or if there is no path between the countries,
-   * the function returns an empty List . Each element of the list must be a String representing one2
-   * step in a longer path in the format: starting_country --> ending_country (DISTANCE_IN_KM.), eg:
+   * If either of the countries does not exist or if there is no path between the
+   * countries,
+   * the function returns an empty List . Each element of the list must be a
+   * String representing one2
+   * step in a longer path in the format: starting_country --> ending_country
+   * (DISTANCE_IN_KM.), eg:
    * Thailand --> Burma (573 km.)
    */
   public List<String> findPath(String country1, String country2, Graph graph) {
@@ -91,10 +109,10 @@ public class IRoadTrip {
     List<String> path = new ArrayList<String>();
     // use bfs to find path if edge paths are all the same
     // if edge paths are not the same, use dijkstra's algorithm
-    // set up a table and use directed, weighted graph 
+    // set up a table and use directed, weighted graph
 
-    //list to store path 
-    System.out.println(country1 + country2 + graph);
+    // list to store path
+    // System.out.println(country1 + country2 + graph);
     path = graph.dijkstra(country1, country2);
     for (String s : path) {
       System.out.println(s);
@@ -109,14 +127,19 @@ public class IRoadTrip {
   }
 
   /*
-   * This function allows a user to interact with your implementation on the console.
-   * Through this function, your implementation is required to receive and validate the names
-   * of two countries from a user. The country names must be validated — i.e. your implementation must not
-   * accept invalid names. Once two valid country names have been entered by the user, the implementation
-   * must print the path between those countries if such a path exists. A sample user interaction is shown
+   * This function allows a user to interact with your implementation on the
+   * console.
+   * Through this function, your implementation is required to receive and
+   * validate the names
+   * of two countries from a user. The country names must be validated — i.e. your
+   * implementation must not
+   * accept invalid names. Once two valid country names have been entered by the
+   * user, the implementation
+   * must print the path between those countries if such a path exists. A sample
+   * user interaction is shown
    * below, with user input in bold italics:
    */
-  public void acceptUserInput() {
+  public String acceptUserInput() {
     // create scanner object
     Scanner scanner = new Scanner(System.in);
     // prompt user for input
@@ -139,33 +162,38 @@ public class IRoadTrip {
     }
 
     // print countries for testing
-    System.out.println("First country: " + country1);
-    System.out.println("Second country: " + country2);
+    // System.out.println("First country: " + country1);
+    // System.out.println("Second country: " + country2);
     // close scanner
     scanner.close();
+    // return countries separated by underscore
+    return country1 + "_" + country2;
   }
 
   public static void main(String[] args) {
     System.out.println("Starting IRoadTrip...");
     IRoadTrip roadTrip = new IRoadTrip(args);
-    // get hashmaps 
+    // get hashmaps
     HashMap<String, Integer> capDistMap = roadTrip.capDistMap;
     HashMap<String, String> stateNameMap = roadTrip.stateNameMap;
     HashMap<String, List<String>> bordersMap = roadTrip.bordersMap;
     HashMap<String, String> reverseStateMap = roadTrip.reverseStateMap;
     HashMap<String, String> edgeCases = roadTrip.edgeCases;
     HashMap<String, String> edgeCasesbyString = roadTrip.edgeCasesbyString;
-   //roadTrip.acceptUserInput();
     System.out.println("Starting graph...");
-    Graph graph = new Graph();
+    Graph graph = new Graph(capDistMap, stateNameMap, bordersMap, reverseStateMap, edgeCases, edgeCasesbyString);
     System.out.println("Graph object made..");
-    graph.buildGraph(capDistMap, stateNameMap, bordersMap, reverseStateMap, edgeCases, edgeCasesbyString);
-  //  graph.printGraph();
+    graph.buildGraph();
+    // graph.printGraph();
     System.out.println("Graph built...");
-
-    // roadTrip.getDistance("Spain", "France");
+    String inputCountries = roadTrip.acceptUserInput();
+    // string split to get input countries
+    String[] countries = inputCountries.split("_");
+    String country1 = countries[0];
+    String country2 = countries[1];
+    int distance = roadTrip.getDistance(country1, country2, graph);
+    System.out.println("This distance between " + country1 + " and " + country2 + " is " + distance + " km");
     roadTrip.findPath("", "France", graph);
     System.out.println("Path found...");
   }
 }
-
